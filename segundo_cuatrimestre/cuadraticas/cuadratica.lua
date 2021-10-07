bad6c0a8acd176232a31db211cfd093ca9383353
irastorzatobias <tobirastorza@gmail.com>
2021-10-07 20:40:40 -0300

    cuadratica, falta corte y, creciente o dec

--- segundo_cuatrimestre/cuadraticas/cuadratica.lua
+++ segundo_cuatrimestre/cuadraticas/cuadratica.lua
@@ -5,10 +5,7 @@ function operadoresCuadratica()
     num1 = valFloat('Ingrese el valor de a: ')
     num2 = valFloat('Ingrese el valor de b: ')
     num3 = valFloat('Ingrese el valor de c: ')
-    op1 = valOp('Operacion entre a y b: ')
-    op2 = valOp('Operacion entre b y c: ')
-    -- print(a..'x2'..opA..b..'x'..opB..c)
-    return num1,num2,num3,op1,op2
+    return num1,num2,num3
 end
 
 function discriminante(a,b,c)
@@ -29,18 +26,25 @@ end
 
 
 function resolveCuadratica()
-    a,b,c,opA,opB = operadoresCuadratica()
+    a,b,c = operadoresCuadratica()
     resDiscriminante = discriminante(a,b,c)
     cDiscriminante = checkDiscriminante(resDiscriminante)
     if cDiscriminante == 1 then
-        x1 = ((-b) + (b ^ 0.5) - (4*a*c)) / 2 
-        return x1
+        x1 = ((-b) + (math.sqrt(resDiscriminante))) / (2 * a)
+        xv = (-b) / (2 * a)
+        yv = a*(xv^2) + b * xv + c 
+        print('X1: '..x1..'\nXV: '..xv..'\nYV: '..yv)
     elseif cDiscriminante == 2 then
-        -- print(resDiscriminante)
-        -- print(resDiscriminante ^ 0.5)
-        -- x1 = (-b) + ((resDiscriminante ^ 0.5) / (2 * a))
-        -- x2 = (-b) - (((b ^ 0.5) - (4*a*c)) / 2)
+        x1 = ((-b) + (math.sqrt(resDiscriminante))) / (2 * a) 
+        x2 = ((-b) - (math.sqrt(resDiscriminante))) / (2 * a)
+        xv = (-b) / (2 * a)
+        yv = a*(xv^2) + b * xv + c
+        print('X1: '..x1,'X2: '..x2..'\nXV: '..xv..'\nYV: '..yv)
+    else
+        print('No tiene solucion en los reales')
     end
 end 
  
+
 resolveCuadratica()
+
